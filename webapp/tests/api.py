ff8ccacf4238ed5d2b7f298c56d6baeea5abeaab
Eric Stadelman <ericstadelman@eduroam-168-82.dyn.carleton.edu>
2018-10-16 22:52:52 -0500

    Phase 4 Complete

---
+++ webapp/tests/api.py
@@ -0,0 +1,13 @@
+import csv
+
+class api:
+	
+
+	def __init__(self, football_data_file):
+		pass
+
+	def get_stats(self, start_date = 'NULL' , end_date= 'NULL', team_list = 'NULL' , stat_list = 'NULL'):
+		pass
+
+	def get_odds(self, date = 'NULL' , team_list = 'NULL' , company_list = 'NULL'):
+		pass
